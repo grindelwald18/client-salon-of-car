5628c214dad3f2ee77d66f76bf3850872192370d
Sergey <103059403+grindelwald18@users.noreply.github.com>
2023-12-02 23:45:14 +0300

    install react-router-dom

---
+++ src/pages/BrandsPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BrandsTable from '../components/BrandsTable';
+
+
+export default function BrandsPage() {
+
+    return (
+        <>
+        <h1>Страница брендов</h1>
+            <BrandsTable />
+        </>
+    )
+
+}
