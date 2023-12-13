35b4d69c4b13f924c3db75aef355d0aad9ecf978
Sergey <103059403+grindelwald18@users.noreply.github.com>
2023-12-13 23:44:52 +0300

    add page Brabd

--- src/components/BrandsTable.js
+++ src/components/BrandsTable.js
@@ -1,4 +1,3 @@
-
 import React, {useEffect, useState} from 'react';
 import brandService from '../services/BrandsService';
 import {Table, Empty, Button, Form, Input, Modal, Spin} from 'antd';
@@ -9,24 +8,94 @@ export default function BrandsTable() {
 
     const [brands, setBrands] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [editingRecord, setEditingRecord] = useState(null);
     const [form] = Form.useForm();
 
     useEffect(() => {
         (async function getBrands() {
             try {
-                const data = await brandService.getBrands();
+                const data = await brandService.fetchBrands();
                 setBrands(data);
                 setIsLoading(false);
-                console.log(data);
             } catch (error) {
                 setIsLoading(false);
             }
         })();
     }, []);
 
+    const handleDelete = async (id, brand) => {
+        try {
+            await brandService.deleteBrand(id);
+            setBrands(prevBrands => prevBrands.filter(item => item.id !== id));
+            toast.success(`Бренд ${brand} успешно удалён`);
+        } catch (error) {
+            toast.error(`Не удалось удалить бренд: ${brand}`);
+        }
+    };
 
+    const handleEdit = record => {
+        form.setFieldsValue(record);
+        setIsModalOpen(true);
+        setEditingRecord(record);
+    };
 
-const columns = [
+    const handleFormSubmit = () => {
+        form
+            .validateFields()
+            .then(values => {
+                const updatedBrand = {...editingRecord, ...values};
+                brandService
+                    .updateBrand(updatedBrand)
+                    .then(() => {
+                        setBrands(prevBrands =>
+                            prevBrands.map(item => (item.id === updatedBrand.id ? updatedBrand : item))
+                        );
+                        setIsModalOpen(false);
+                        setEditingRecord(null);
+                        toast.success(`Бренд ${updatedBrand.brand} успешно отредактирован`);
+                    })
+                    .catch(() => {
+                        toast.error(`Не удалось отредактировать бренд: ${updatedBrand.brand}`);
+                        handleCancel();
+                    });
+            })
+            .catch(() => toast.error('Не удалось отредактировать бренд'));
+    };
+
+    const handleCancel = () => {
+        form.resetFields();
+        setIsModalOpen(false);
+        setEditingRecord(null);
+    };
+
+    const handleAdd = () => {
+        form.setFieldsValue({id: '', brand: ''});
+        setIsModalOpen(true);
+        setEditingRecord(null);
+    };
+
+    const handleSave = () => {
+        form
+            .validateFields()
+            .then(values => {
+                const newBrand = {brand: values.brand};
+                brandService
+                    .saveBrand(newBrand)
+                    .then(response => {
+                        setBrands(prevBrands => [...prevBrands, response]);
+                        setIsModalOpen(false);
+                        toast.success(`Бренд ${response.brand} успешно добавлен`);
+                    })
+                    .catch(() => {
+                        toast.error(`Не удалось добавить бренд`);
+                        handleCancel();
+                    });
+            })
+            .catch(() => toast.error('Не удалось добавить бренд'));
+    };
+
+    const columns = [
         {
             title: 'id',
             dataIndex: 'id',
@@ -37,24 +106,69 @@ const columns = [
             dataIndex: 'brand',
             key: 'brand',
         },
-        
-       
+        {
+            title: 'Действия',
+            key: 'actions',
+            render: (text, record) => (
+                <>
+                    <Button
+                        onClick={() => handleEdit(record)}
+                        style={{border: 'none', color: 'orange'}}
+                    >
+                        Редактировать
+                    </Button>
+                    <Button danger type="text" onClick={() => handleDelete(record.id, record.brand)}>
+                        Удалить
+                    </Button>
+                </>
+            ),
+        },
     ];
+
+    const modalTitle = editingRecord ? 'Редактирование бренда' : 'Добавление бренда';
 
     return (
         <>
-       
+            <Modal
+                title={modalTitle}
+                visible={isModalOpen}
+                onCancel={handleCancel}
+                footer={[
+                    <Button key="cancel" onClick={handleCancel}>
+                        Отменить
+                    </Button>,
+                    editingRecord ? (
+                        <Button key="save" type="primary" onClick={handleFormSubmit}>
+                            Сохранить
+                        </Button>
+                    ) : (
+                        <Button key="save" type="primary" onClick={handleSave}>
+                            Добавить
+                        </Button>
+                    ),
+                ]}
+            >
+                <Form form={form} initialValues={editingRecord}>
+                    <Form.Item name="id" label="ID">
+                        <Input disabled/>
+                    </Form.Item>
+                    <Form.Item name="brand" label="Brand" rules={[{required: true, message: 'Введите бренд'}]}>
+                        <Input/>
+                    </Form.Item>
+                </Form>
+            </Modal>
 
             {isLoading ? (
                 <Spin/>
             ) : brands.length > 0 ? (
                 <Table dataSource={brands} columns={columns} pagination={false}/>
-                
             ) : (
                 <Empty description="Список брендов пуст"/>
             )}
 
-           
+            <Button onClick={handleAdd} type="primary" style={{marginBottom: '16px'}}>
+                Добавить
+            </Button>
         </>
     );
 
--- src/services/BrandsService.js
+++ src/services/BrandsService.js
@@ -1,15 +1,66 @@
 import axios from 'axios';
 
-class BrandsService {
+class BrandService {
 
-    async getBrands() {
+    async fetchBrands() {
         try {
-            const response = await axios.get('http://localhost:8080/brand');
+            const response = await axios.get('http://localhost:8080/brand')
+            return response.data;
+            // const token = JSON.parse(localStorage.getItem('jwtToken'))
+            // const requestOptions = {
+            //     method: 'GET',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //         'Authorization': `Bearer ${token}`
+            //     },
+            // };
+            //
+            // fetch('http://localhost:8080/brands', requestOptions)
+            //     .then(response => {
+            //         if (!response.ok) {
+            //             throw new Error('Error sending request');
+            //         }
+            //         return response.json();
+            //     })
+            //     .then(data => {
+            //         return data;
+            //     })
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async setHeaders() {
+        const token = JSON.parse(localStorage.getItem('jwtToken'))
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    }
+
+    async deleteBrand(id) {
+        try {
+            const response = await axios.delete(`http://localhost:8080/brand/${id}`);
             return response.data;
         } catch (error) {
-            // console.log(error);
+            throw error;
+        }
+    }
+
+    async updateBrand(brand) {
+        try {
+            const response = await axios.put(`http://localhost:8080/brand/${brand.id}`, brand);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async saveBrand(brand) {
+        try {
+            const response = await axios.post(`http://localhost:8080/brand/add`, brand);
+            return response.data;
+        } catch (error) {
             throw error;
         }
     }
 }
-export default new BrandsService();
+
+export default new BrandService();
